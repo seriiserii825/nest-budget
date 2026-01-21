9218b5f42cb16c805c2f62fa5fe82b7ed31b0321
serii burduja <seriiburduja@gmail.com>
2026-01-21 10:04:21 +0200

    feat: main ts catch

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3300);
 }
-bootstrap();
+bootstrap().catch((err) => {
+  console.error('Error during application bootstrap:', err);
+});

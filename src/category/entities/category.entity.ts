8277fca02e4487252d82a1303bf60ad04c7de491
serii burduja <seriiburduja@gmail.com>
2026-01-21 16:31:37 +0200

    feat: category entity and migration

--- src/category/entities/category.entity.ts
+++ src/category/entities/category.entity.ts
@@ -1,1 +1,22 @@
-export class Category {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('categories')
+export class Category {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}

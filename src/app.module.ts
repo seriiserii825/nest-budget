35aba006d7b0d828808b11c93d7a6555eaf3c1a4
serii burduja <seriiburduja@gmail.com>
2026-01-21 10:53:03 +0200

    feat: transaction and auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { CategoryModule } from './category/category.module';
+import { AuthModule } from './auth/auth.module';
+import { TransactionModule } from './transaction/transaction.module';
 
 @Module({
-  imports: [UserModule, CategoryModule],
+  imports: [UserModule, CategoryModule, AuthModule, TransactionModule],
   controllers: [AppController],
   providers: [AppService],
 })

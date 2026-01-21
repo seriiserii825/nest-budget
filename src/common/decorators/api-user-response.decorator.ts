9c994d77a054602b42caaab8d27baf6e99f1017d
serii <seriiburduja@gmail.com>
2026-01-21 21:57:15 +0200

    feat: user response schame and type

--- src/common/decorators/api-user-response.decorator.ts
+++ src/common/decorators/api-user-response.decorator.ts
@@ -1,13 +1,19 @@
 // src/common/decorators/api-user-response.decorator.ts
 import { applyDecorators } from '@nestjs/common';
-import { ApiOkResponse } from '@nestjs/swagger';
+import { ApiOkResponse, getSchemaPath } from '@nestjs/swagger';
+import { User } from 'src/user/entities/user.entity';
 import { userResponseExample } from 'src/user/examples/user-response.example';
 
 export function ApiUserResponse() {
   return applyDecorators(
     ApiOkResponse({
       schema: {
-        example: userResponseExample,
+        allOf: [
+          { $ref: getSchemaPath(User) },
+          {
+            example: userResponseExample,
+          },
+        ],
       },
     }),
   );

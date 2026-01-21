5e31179f6f9fde239b203affbf6e4b4fc13f262e
serii <seriiburduja@gmail.com>
2026-01-21 22:05:34 +0200

    feat: finished register user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,6 +4,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/user/entities/user.entity';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from 'src/user/dto/create-user.dto';
+import * as argon2 from 'argon2';
 
 @Injectable()
 export class AuthService {
@@ -11,13 +12,25 @@ export class AuthService {
     @InjectRepository(User) private userRepository: Repository<User>,
   ) {}
 
-  async create(createUserDto: CreateUserDto): Promise<User> {
+  async create(
+    createUserDto: CreateUserDto,
+  ): Promise<Pick<User, 'id' | 'email' | 'createdAt' | 'updatedAt'>> {
     const user_exists = await this.isUserExists(createUserDto.email);
     if (user_exists) {
       throw new ConflictException('User with this email already exists');
     }
-    const user = this.userRepository.create(createUserDto);
-    return this.userRepository.save(user);
+    const user = this.userRepository.create({
+      email: createUserDto.email,
+      password: await argon2.hash(createUserDto.password),
+    });
+    await this.userRepository.save(user);
+    // return user without password
+    return {
+      id: user.id,
+      email: user.email,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+    };
   }
 
   findAll() {

3d3e9a3592703f55d05249be68bec633bbbc1b47
serii burduja <seriiburduja@gmail.com>
2026-01-21 10:21:23 +0200

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.enableCors({
+    origin: process.env.NODE_ENV === 'production' ? 'https://example.com' : '*',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3300);
 }
 bootstrap().catch((err) => {

7c0cb18226fb8c2d3b446e2dc532f20026624065
serii burduja <seriiburduja@gmail.com>
2026-01-22 12:49:16 +0200

    feat: create category

--- src/category/dto/create-category.dto.ts
+++ src/category/dto/create-category.dto.ts
@@ -1,22 +1,33 @@
 import { ApiProperty, PickType } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CategoryDto {
   @ApiProperty({ example: 1, description: 'The ID of the category' })
+  @IsNumber()
   id: number;
+
   @ApiProperty({
     example: 'Groceries',
     description: 'The title of the category',
   })
+  @IsString()
+  @IsNotEmpty()
   title: string;
+
   @ApiProperty({
     example: '2023-10-01T12:00:00Z',
     description: 'Creation date',
   })
+  @IsString()
+  @IsNotEmpty()
   createdAt: Date;
+
   @ApiProperty({
     example: '2023-10-02T12:00:00Z',
     description: 'Last update date',
   })
+  @IsString()
+  @IsNotEmpty()
   updatedAt: Date;
 }
 
